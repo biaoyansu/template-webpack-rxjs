7986efff2af7c20829774d4895f49c708a973d57
Biaoyansu <thehailang@gmail.com>
2019-02-20 00:08:35 +0800

    Init

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import {Observable} from 'rxjs'
 
 let count = 0
 
-const obs = new Observable(o => {
-  o.next(1)
-  o.next(2)
-  o.next(3)
+const obs = new Observable(observer => {
+  try {
+    observer.next(1)
+    observer.next(2)
+    observer.next(3)
+    observer.complete();
+  } catch (e) {
+    observer.error(e);
+  }
 })
 
-obs.subscribe(newValue => console.log(newValue))
+obs.subscribe(
+  newValue => console.log(newValue),
+  error => console.log(error),
+  () => console.log('Completed.')
+)
